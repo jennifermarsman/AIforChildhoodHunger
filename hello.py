ab0859a743037bdcae43cc298496a8bbbceff828
Girinath R <girir@microsoft.com>
2023-09-15 18:48:40 +0530

    data flow between pages

--- hello.py
+++ hello.py
@@ -1,63 +1,111 @@
+import time
 import gradio as gr
 import numpy as np
 import json
 from prototype import chat
 from constants import states
 
-def nextQuestionnaire2(check):
-        prompt2 = check
-        print(prompt2)
-        return {
-            questionnairePage3: gr.update(visible=True),
-            questionnairePage2: gr.update(visible=False)
-        }
-def nextQuestionnaire1(ques1Input, ques2Input, ques3Input, zipCode, kidsBelow5, kidsAbove5Below18):
+def buildInfoAboutUserFromQues1(zipCode, kidsBelow5, kidsAbove5Below18):
+        promptInfo = "My zipcode is " + zipCode + " and have " + kidsBelow5 + " kids below 5 and " + kidsAbove5Below18 + " kids above 5 below 18. "
+        return promptInfo
+
+def buildInfoAboutUserFromQues2(enrolled):
+        promptInfo = "I am enrolled in " + enrolled + ". "
+        return promptInfo
+
+def buildInfoAboutUserFromQues3(householdSize, housholdAbove60, usCitizen, jobOrSelfEmpIncome, otherSourcesIncome, collegeStudies, ageBucket, pregnancyStatus, childrenAgeStatus):
+        promptInfo = "My household size is " + householdSize + " and " + housholdAbove60 + ", for people in my household above 60. " + '.'.join(usCitizen) + ". " + jobOrSelfEmpIncome + " is my income from my job. " + otherSourcesIncome + " is my income from other sources." + collegeStudies + ", for am I enrolled in college." + '.'.join(ageBucket) + ". " + pregnancyStatus + ", for am I pregnant." + childrenAgeStatus + ", for do I have children below 5."
+        return promptInfo
+
+
+def nextQuestionnaire2(check, promptInfo):
+        enrolled = check
+        questionnairePage3 = gr.update(visible=True)
+        questionnairePage2 = gr.update(visible=False)
+        #promptInfo += buildInfoAboutUserFromQues2(enrolled)
+        print(promptInfo)
+        return questionnairePage3, questionnairePage2, enrolled, promptInfo
+
+def nextQuestionnaire1(ques1Input, ques2Input, ques3Input, promptInfo):
         zipCode = ques1Input
         kidsBelow5 = ques2Input
         kidsAbove5Below18 = ques3Input
         # prompt = ques1 + " " + ques2 + " " + ques3
         # print(prompt)
-        prompt = zipCode + " " + kidsBelow5 + " " + kidsAbove5Below18
-        print(prompt)
-        return {
-            questionnairePage2: gr.update(visible=True),
-            introQuestionnaire: gr.update(visible=False),
-            zipCode: zipCode,
-            kidsBelow5: kidsBelow5,
-            kidsAbove5Below18: kidsAbove5Below18
-        }
+        introQuestionnaire = gr.update(visible=False)
+        questionnairePage2 = gr.update(visible=True)
+        promptInfo += buildInfoAboutUserFromQues1(zipCode, kidsBelow5, kidsAbove5Below18)
+        print(promptInfo)
+        return introQuestionnaire, questionnairePage2, zipCode, kidsBelow5, kidsAbove5Below18, promptInfo
 
 def start():
-        return {
-            introQuestionnaire: gr.update(visible=True),
-            introPage: gr.update(visible=False)
-        }
+        introQuestionnaire = gr.update(visible=True)
+        introPage = gr.update(visible=False)
+        return introQuestionnaire, introPage
 
-def startbot():
-        return {
-            botScreen: gr.update(visible=True),
-            questionnairePage3: gr.update(visible=False)
-        }
+def startbot(ques1, ques2, ques3, ques4, ques5, ques6, ques7, ques8, ques9, promptInfo):
+        householdSize = ques1
+        housholdAbove60 = ques2
+        usCitizen = ques3
+        jobOrSelfEmpIncome = ques4
+        otherSourcesIncome = ques5
+        collegeStudies = ques6
+        ageBucket = ques7
+        pregnancyStatus = ques8
+        childrenAgeStatus = ques9
+        promptInfo += buildInfoAboutUserFromQues3(ques1, ques2, ques3, ques4, ques5, ques6, ques7, ques8, ques9)
+        print(promptInfo)
+        botScreen = gr.update(visible=True)
+        questionnairePage3 = gr.update(visible=False)
+        return botScreen, questionnairePage3, householdSize, housholdAbove60, usCitizen, jobOrSelfEmpIncome, otherSourcesIncome, collegeStudies, ageBucket, pregnancyStatus, childrenAgeStatus, promptInfo
+
+def chatInvoke(msg, promptInfo, statesDropdown, chat_history):
+        userMsg = msg
+        prompt = "given the following information about me: " + "I am from the state " + statesDropdown +  ". " + promptInfo + " " + msg
+        response = chat(prompt, statesDropdown)
+        chat_history.append((userMsg, response))
+        print(prompt)
+        print(response)
+        print(chat_history)
+        return "", chat_history
 
  
 with gr.Blocks() as demo:
 
+    #states for questionnaire 1
     zipCode = gr.State(value="")
     kidsAbove5Below18 = gr.State(value="")
     kidsBelow5 = gr.State(value="")
+
+    #states for questionnaire 2
+    enrolled = gr.State(value="")
+
+    #states for questionnaire 3
+    householdSize = gr.State(value="")
+    housholdAbove60 = gr.State(value="")
+    usCitizen = gr.State(value="")
+    jobOrSelfEmpIncome = gr.State(value="")
+    otherSourcesIncome = gr.State(value="")
+    collegeStudies = gr.State(value="")
+    ageBucket = gr.State(value="")
+    pregnancyStatus = gr.State(value="")
+    childrenAgeStatus = gr.State(value="")
+
+    promptInfo = gr.State(value="")
+
+    response = gr.State(value="")
+    state = gr.State(value="")
 
     with gr.Group(visible=False) as botScreen:
         with gr.Blocks() as sosChatBot:
             with gr.Row():
                 statesArray = states
-                statesDropdown = gr.Dropdown(
-                    statesArray, label="States", info="Choose your state"
-                ),
+                statesDropdown = gr.Dropdown(statesArray, label="States", info="Choose your state")
             with gr.Row():
                 chatbot = gr.Chatbot(bubble_full_width = False)
                 msg = gr.Textbox()
                 clear = gr.ClearButton([msg, chatbot])
-                msg.submit(chat, [msg, chatbot], [msg, chatbot])
+                msg.submit(chatInvoke, [msg, promptInfo,statesDropdown, chatbot], [msg, chatbot])
                 #chat_interface = gr.ChatInterface(fn=chat, chatbot=chatbot)
 
     with gr.Group(visible=False) as questionnairePage3:
@@ -75,7 +123,7 @@ with gr.Blocks() as demo:
         ques8 = gr.Radio(["Yes", "No"], label="8. Are any of the members of your household pregnant, or was pregnant in the last 6 months?")
         ques9 = gr.Radio(["Yes", "No"], label="9. Are any of the members of your household an infant or child up that hasn’t yet had their 5th birthday?")
         aiHelper = gr.Button("Send to AI Helper")
-        aiHelper.click(startbot, inputs=[], outputs=[botScreen,questionnairePage3])
+        aiHelper.click(startbot, inputs=[ques1,ques2,ques3,ques4,ques5,ques6,ques7,ques8,ques9, promptInfo], outputs=[botScreen,questionnairePage3,householdSize,housholdAbove60,usCitizen,jobOrSelfEmpIncome,otherSourcesIncome,collegeStudies,ageBucket,pregnancyStatus,childrenAgeStatus,promptInfo])
 
     with gr.Group(visible=False) as questionnairePage2:
 
@@ -88,7 +136,7 @@ with gr.Blocks() as demo:
         gr.Markdown("<p style='text-align: center;'>{}</p>".format("While each program has a unique application process, our questionnaire (9 questions) can tell you which programs you qualify for — then give you the links or phone number number"))
         gr.Markdown("<p style='text-align: center;'>{}</p>".format("All of your answers are private to you, and will not be shared with anybody."))
         questionnairePage2Button = gr.Button("Start questionnaire")
-        questionnairePage2Button.click(nextQuestionnaire2, inputs=[check], outputs=[questionnairePage2,questionnairePage3])
+        questionnairePage2Button.click(nextQuestionnaire2, inputs=[check, promptInfo], outputs=[questionnairePage3,questionnairePage2, enrolled, promptInfo])
 
     with gr.Group(visible=False) as introQuestionnaire:
 
@@ -99,7 +147,7 @@ with gr.Blocks() as demo:
         ques2 = gr.Textbox(label="2. How many kids do you have below age 5?", info="More programs may be available depending on your answer")
         ques3 = gr.Textbox(label="3. How many kids do you have aged 5-18?", info="More programs may be available depending on your answer.")
         nextButton = gr.Button("Show nearby programs")
-        nextButton.click(nextQuestionnaire1, inputs = [ques1.input, ques2.input, ques3.input, zipCode, kidsBelow5, kidsAbove5Below18],outputs=[introQuestionnaire,questionnairePage2,zipCode,kidsBelow5,kidsAbove5Below18] )
+        nextButton.click(nextQuestionnaire1, inputs = [ques1, ques2, ques3, promptInfo],outputs=[introQuestionnaire, questionnairePage2,zipCode,kidsBelow5,kidsAbove5Below18,promptInfo] )
 
     with gr.Group(visible=True) as introPage:
       
